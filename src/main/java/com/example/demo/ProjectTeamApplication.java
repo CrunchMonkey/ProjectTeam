aac1abca7170d4eba1fa27c783cb70bb63317a53
이승렬 <dltmdfuf95@naver.com>
2023-08-10 15:01:40 +0900

    기본적인 설정

--- src/main/java/com/example/demo/ProjectTeamApplication.java
+++ src/main/java/com/example/demo/ProjectTeamApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @MapperScan(basePackages = {"com.example.demo.mapper"})
 public class ProjectTeamApplication {
 
--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -1,8 +1,31 @@
 package com.example.demo.controller;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class testController {
 	
+	@GetMapping(value = "/")
+	public String test() throws Exception {
+		String jsp = "main";
+		return jsp;
+	}
+	
+	
 }
+//@GetMapping(value = "/")
+//@ResponseBody
+//public JSONObject test() throws Exception {
+//	JSONObject obj = new JSONObject();
+//	JSONParser parser = new JSONParser();
+//	obj = (JSONObject) parser.parse("{\"레모나\" : 12}");
+//	return obj;
+//}
+
+
+
